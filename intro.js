2e0866a05abed93293caca7ca76ea455418948eb
Charles13064 <Charlesmuhanji13064@gmail.com>
2026-05-15 17:46:32 +0300

    Initial commit: add assignment file

---
+++ intro.js
@@ -0,0 +1,6 @@
+const name = "Charles Muhanji";
+const age = 20;
+const faveProgLang = "JavaScript";
+
+console.log(`My name is ${name}, I am ${age} years old and my favourite programming language is ${faveProgLang}.`);
+
